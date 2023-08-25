808c1f9f2d18b4b2289828ec870766b2b8f53f8d
Douglas Speck <speck.empresarial@gmail.com>
2023-08-25 18:05:36 -0300

    Moved to PHP

---
+++ assets/php/header.php
@@ -0,0 +1,37 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="">home</a></li>
+            <li><a href="sobre/">sobre nós</a>
+                <ul>
+                    <li><a href="sobre/historia/">nossa história</a></li>
+                    <li><a href="/sobre/gente/">nossa gente</a></li>
+                    <li><a href="sobre/cultura/">nosso jeito</a></li>
+                </ul>
+            </li>
+            <li><a href="eventos/">eventos</a>
+                <ul>
+                    <li><a href="eventos/assembleias/">assembleias</a></li>
+                    <li><a href="eventos/quermecc/">quermecc</a></li>
+                    <li><a href="eventos/campeonato-de-sinuca/">campeonato de sinuca</a></li>
+                    <li><a href="eventos/campeonato-de-truco/">campeonato de truco</a></li>
+                </ul>
+            </li>
+            <li><a href="academico/">recursos acadêmicos</a>
+                <ul>
+                    <li><a href="academico/cursos/">cursos</a></li>
+                    <li><a href="academico/materiais/">materiais</a></li>
+                    <li><a href="academico/guias/">guias</a></li>
+                </ul>
+            </li>
+            <li><a href="resenha/">resenha</a>
+                <ul>
+                    <li><a href="resenha/sinuca/">sinuca</a></li>
+                    <li><a href="resenha/minecraft.html">minecraft</a></li>
+                    <li><a href="resenha/xadrez/">equipe de xadrez</a></li>
+                </ul>
+            </li>
+            <li><a href="contato.html">contato</a></li>
+        </ul>
+    </nav>
+</header>
